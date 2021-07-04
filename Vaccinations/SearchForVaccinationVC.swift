fb7ddfdac84507b84db0f3adcd4ccea46c09e405
Harshal Pathak <harshalvpathak@gmail.com>
2021-07-04 11:11:52 +0530

    Search View Completed - With Pincode validation and Date picker added to Textfields

--- Vaccinations/SearchForVaccinationVC.swift
+++ Vaccinations/SearchForVaccinationVC.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class SearchForVaccinationVC: UIViewController {
+class SearchForVaccinationVC: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtPincode: UITextField!
+    @IBOutlet weak var txtDateForVaccination: UITextField!
+    
+    let datePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        datePicker.preferredDatePickerStyle = .wheels
+        txtPincode.delegate = self
+        txtDateForVaccination.addCancelDoneOnKeyboardWithTarget(self, cancelAction: #selector(cancelDatePicker), doneAction: #selector(donedatePicker))
+        setDatePicker()
+    }
+    
+    func setDatePicker() {
+        datePicker.datePickerMode = UIDatePicker.Mode.date
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_IN") // set locale to reliable US_POSIX
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        datePicker.minimumDate = Date()
+        self.txtDateForVaccination.inputView = datePicker
+    }
+    
+    @objc func donedatePicker(){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM-yyyy"
+        txtDateForVaccination.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    @objc func cancelDatePicker(){
+        self.txtDateForVaccination.text = ""
+        self.view.endEditing(true)
     }
 
+    @IBAction func actionSearch(_ sender: UIButton) {
+        
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == txtPincode {
+            let maxLength = 6
+                let currentString: NSString = (textField.text ?? "") as NSString
+                let newString: NSString =
+                    currentString.replacingCharacters(in: range, with: string) as NSString
+                return newString.length <= maxLength
+        }
+        return true
+    }
 
 }
 
